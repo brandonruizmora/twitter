5fe1a7959f95a97396b577d2d2959c6d9055aa30
brandonruizmora <brandon_ruizmor@outlook.es>
2022-04-22 18:42:45 -0500

    datecreated and lastupdate should be created when instancing

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -2,12 +2,14 @@ const User = require("../../app/models/User");
 
 describe('User unit test', () => {
     test('Creation of User model', () => {
-        const user = new User(122332, 'pandaimpetuoso7', 'Brandon', 'Hey whatsup', '01012022', '02022022');
+        const user = new User(122332, 'pandaimpetuoso7', 'Brandon', 'Hey whatsup');
         expect(user.id).toBe(122332);
         expect(user.username).toBe('pandaimpetuoso7');
         expect(user.name).toBe('Brandon');
         expect(user.bio).toBe('Hey whatsup');
-        expect(user.dateCreated).toBe('01012022');
-        expect(user.lastUpdated).toBe('02022022');
+        expect(user.dateCreated).toBeTruthy();
+        expect(user.dateCreated instanceof Date).toBe(true);
+        expect(user.lastUpdated).toBeTruthy();
+        expect(user.lastUpdated instanceof Date).toBe(true);
     });
 });
