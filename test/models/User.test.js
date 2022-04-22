75004ee7e6c9a696fa6be9f63b9171610419ed08
brandonruizmora <brandon_ruizmor@outlook.es>
2022-04-22 18:03:50 -0500

    added user class and refactor

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -1,10 +1,13 @@
+const User = require("../../app/models/User");
+
 describe('User unit test', () => {
     test('Creation of User model', () => {
-        const user = new User(122332, 'pandaimpetuoso7', 'Brandon', new Date(), new Date());
+        const user = new User(122332, 'pandaimpetuoso7', 'Brandon', 'Hey whatsup', '01012022', '02022022');
         expect(user.id).toBe(122332);
         expect(user.username).toBe('pandaimpetuoso7');
         expect(user.name).toBe('Brandon');
-        expect(user.dateCreated).toBe(new Date());
-        expect(user.lastUpdated).toBe(new Date());
+        expect(user.bio).toBe('Hey whatsup');
+        expect(user.dateCreated).toBe('01012022');
+        expect(user.lastUpdated).toBe('02022022');
     });
 });
