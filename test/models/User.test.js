e8310229f5f018c410e406d55f59e61aa78de588
brandonruizmora <brandon_ruizmor@outlook.es>
2022-04-22 18:54:51 -0500

    added requirement 3

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -1,15 +1,27 @@
 const User = require("../../app/models/User");
 
 describe('User unit test', () => {
-    test('Creation of User model', () => {
+    test('Requirement 1: Creation of User model', () => {
         const user = new User(122332, 'pandaimpetuoso7', 'Brandon', 'Hey whatsup');
         expect(user.id).toBe(122332);
         expect(user.username).toBe('pandaimpetuoso7');
         expect(user.name).toBe('Brandon');
         expect(user.bio).toBe('Hey whatsup');
+    });
+
+    test('Requirement 2: User dateCreated and lastUpdated created when instancing', () => {
+        const user = new User(122332, 'pandaimpetuoso7', 'Brandon', 'Hey whatsup');
         expect(user.dateCreated).toBeTruthy();
         expect(user.dateCreated instanceof Date).toBe(true);
         expect(user.lastUpdated).toBeTruthy();
         expect(user.lastUpdated instanceof Date).toBe(true);
+    });
+
+    test('Requirement 3: User name, bio, dateCreated, lastUpdate getters', () => {
+        const user = new User(122332, 'pandaimpetuoso7', 'Brandon', 'Hey whatsup');
+        expect(user.getUsername).toBe('pandaimpetuoso7');
+        expect(user.getBio).toBe('Hey whatsup');
+        expect(user.getDateCreated).not.toBeUndefined();
+        expect(user.getLastUpdated).not.toBeUndefined();
     });
 });
