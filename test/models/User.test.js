f1a5f68efefba8c7f6634740999410dcf1e6c55e
brandonruizmora <brandon_ruizmor@outlook.es>
2022-04-22 19:00:00 -0500

    added requirement 4

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -1,6 +1,7 @@
 const User = require("../../app/models/User");
 
 describe('User unit test', () => {
+
     test('Requirement 1: Creation of User model', () => {
         const user = new User(122332, 'pandaimpetuoso7', 'Brandon', 'Hey whatsup');
         expect(user.id).toBe(122332);
@@ -24,4 +25,13 @@ describe('User unit test', () => {
         expect(user.getDateCreated).not.toBeUndefined();
         expect(user.getLastUpdated).not.toBeUndefined();
     });
+
+    test('Requirement 4: User username, bio, setters', () => {
+        const user = new User(122332, 'pandaimpetuoso7', 'Brandon', 'Hey whatsup');
+        user.setUsername = 'pandita';
+        user.setBio = 'Hola a todos!';
+        expect(user.getUsername).toBe('pandita');
+        expect(user.getBio).toBe('Hola a todos!');
+    });
+
 });
